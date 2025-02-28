2ee9134e8cea37b6f04ba455695dc811a475e4f1
Gino <gino.doub@gmail.com>
2025-02-28 17:57:04 +0100

    modifications dans Channel.hpp | Ajout de Channel.cpp

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -10,20 +10,20 @@
 
 class Channel {
 private:
-    std::string name;               // Nom du channel
-    std::string topic;              // Sujet du channel (TOPIC)
-    std::set<int> clients;          // Clients dans le channel (représentés par leur fd)
-    std::set<int> bannedClients;    // Clients bannis (représentés par leur fd)
-    std::set<int> operators;        // Utilisateurs avec les droits d'admin
-    std::string mode;               // Mode du channel (ex : "invite-only", "moderated", etc.)
-    size_t maxClients;              // Nombre maximum de clients dans le channel
+    std::string _name;               // Nom du channel
+    std::string _topic;              // Sujet du channel (TOPIC)
+    std::set<int> _clients;          // Clients dans le channel (représentés par leur fd)
+    std::set<int> _bannedClients;    // Clients bannis (représentés par leur fd)
+    std::set<int> _operators;        // Utilisateurs avec les droits d'admin
+    std::string _mode;               // Mode du channel (ex : "invite-only", "moderated", etc.)
+    size_t _maxClients;              // Nombre maximum de clients dans le channel
 
 public:
     // Constructeur
     Channel(const std::string& channelName, size_t maxClients = 50, const std::string& mode = "");
 
     // Ajouter un client au channel
-    void addClient(int fd, const std::string& username);
+    void addClient(int fd);
 
     // Retirer un client du channel (kick)
     void removeClient(int fd);
@@ -64,8 +64,8 @@ public:
     // Afficher les informations des clients dans le channel (juste leurs pseudo ici)
     void printClients() const;
 
-    // Retourner le Client par son pseudo
-    Client* getClientByUsername(const std::string& username);
+	// Retourner le nombre de clients connectés au channel
+    size_t Channel::getClientCount() const;
 };
 
 #endif
