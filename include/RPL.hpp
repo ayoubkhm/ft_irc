fcea664014a8d4b42730dc5b5905fa9c04ab49e8
Gino <gino.doub@gmail.com>
2025-03-27 16:46:12 +0100

    3 RPL en + quand un client se register

--- include/RPL.hpp
+++ include/RPL.hpp
@@ -5,10 +5,10 @@ void	sendServerRpl(int const client_fd, std::string client_buffer);
 
 # define user_id(nickname, username) (":" + nickname + "!" + username + "@localhost")
 
-# define RPL_WELCOME(user_id, nickname) (":localhost 001 " + nickname + " :Welcome to the Internet Relay Network " + user_id + "\r\n")
-//# define RPL_YOURHOST(client, servername, version) (":localhost 002 " + client + " :Your host is " + servername + " (localhost), running version " + version + "\r\n")
-//# define RPL_CREATED(client, datetime) (":localhost 003 " + client + " :This server was created " + datetime + "\r\n")
-//# define RPL_MYINFO(client, servername, version, user_modes, chan_modes, chan_param_modes) (":localhost 004 " + client + " " + servername + " " + version + " " + user_modes + " " + chan_modes + " " + chan_param_modes + "\r\n")
+# define RPL_WELCOME(user_id, nickname) (":localhost 001 " + nickname + " :Welcome to the Internet Relay Network " + user_id + "\r")
+# define RPL_YOURHOST(client, servername, version) (":localhost 002 " + client + " :Your host is " + servername + " (localhost), running version " + version + "\r")
+# define RPL_CREATED(client, datetime) (":localhost 003 " + client + " :This server was created " + datetime + "\r")
+# define RPL_MYINFO(client, servername, version, user_modes, chan_modes, chan_param_modes) (":localhost 004 " + client + " " + servername + " " + version + " " + user_modes + " " + chan_modes + " " + chan_param_modes + "\r\n")
 //# define RPL_ISUPPORT(client, tokens) (":localhost 005 " + client + " " + tokens " :are supported by this server\r\n")
 
 # define ERR_UNKNOWNCOMMAND(client, command) (":localhost 421 " + client + " " + command + " :Unknown command\r\n")
--- include/Server.hpp
+++ include/Server.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <ctime>
 #include <poll.h>
 #include <unistd.h>
 #include <iostream>
@@ -19,7 +20,7 @@
 class Server
 {
 public:
-    Server(int port, const std::string &password);
+    Server(int port, const std::string &password, struct tm *timeinfo);
     ~Server();
     Server(Server const& copy);
     Server &operator=(Server const& copy);    
@@ -29,6 +30,8 @@ public:
     void removeChannel(const std::string &channelName);
     void joinChannel(int fd, const std::string &channelName);
     void kickClient(int fd, const std::string &channelName, int targetFd);
+    std::string getDateTime() const;
+    void setDatetime(struct tm *timeinfo);
     void broadcastToChannel(const std::string &channelName, const std::string &message, int senderFd);
     int getFdByNickname(const std::string &nickname);
     Client* getClientByFd(int fd);
@@ -40,6 +43,7 @@ private:
     std::vector<struct pollfd> pollfds;
     int port;
     std::string password;
+    std::string _datetime;
     std::map<int, Client*> _ClientBook;  // Clé = FD, valeur = Client*
     std::map<std::string, Channel> _Channels;
 
