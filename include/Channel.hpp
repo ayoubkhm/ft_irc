5992d97846a9aa621037c5d63cc003957fffe93d
Gino <gino.doub@gmail.com>
2025-03-31 15:39:23 +0200

    remise au propre des .hpp

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -12,16 +12,16 @@
 class Channel
 {
 private:
+    int _userLimit;
+    bool _inviteOnly;
+    bool _topicRestricted;
     std::string _name;
     std::string _topic;
+    std::string _key;
     std::set<unsigned int> _clients;      // On stocke désormais l'ID unique des clients
     std::set<unsigned int> _operators;
     std::set<unsigned int> _invitedIds;     // Anciennement _invitedFd, on stocke ici les IDs invités
     size_t _maxClients;
-    bool _inviteOnly;
-    bool _topicRestricted;
-    std::string _key;
-    int _userLimit;
 
 public:
     Channel(const std::string& channelName, size_t maxClients = 1024);
@@ -29,32 +29,35 @@ public:
     Channel(Channel const& copy);
     Channel &operator=(Channel const& copy);
 
+    // Setters
+    void setInviteOnly(bool inviteOnly);
+    void setKey(const std::string& key);
+    void setTopicRestricted(bool topicRestricted);
+    void setUserLimit(int limit);
+    void setTopic(const std::string& newTopic);
+
+    //Getters
+    bool getInviteOnly() const;
+    bool getTopicRestricted() const;
+    const std::string& getKey() const;
+    const std::string& getTopic() const;
+    const std::set<unsigned int>& getClientIds() const;      // Renommage de la méthode pour refléter qu'on récupère les IDs
+    size_t getMaxClients() const;
+    size_t getClientCount() const;
+
+    //Méthodes clients
     bool addClient(unsigned int clientId);
-    void addInvitedClient(unsigned int clientId);
-    void removeInvitedClient(unsigned int clientId);
     void removeClient(unsigned int clientId);
     bool isClientInChannel(unsigned int clientId) const;
-    bool isClientInvited(unsigned int clientId) const;
-    bool isOperator(unsigned int clientId) const;
+    void printClients() const;
+
+    //Méthodes operator
     void addOperator(unsigned int clientId);
     void removeOperator(unsigned int clientId);
-    void setTopic(const std::string& newTopic);
-    const std::string& getTopic() const;
-    size_t getMaxClients() const;
-    void printClients() const;
-    size_t getClientCount() const;
-    // Renommage de la méthode pour refléter qu'on récupère les IDs
-    const std::set<unsigned int>& getClientIds() const;
+    bool isOperator(unsigned int clientId) const;
 
-    // Getters et setters pour les modes
-    void setInviteOnly(bool inviteOnly);
-    bool getInviteOnly() const;
-
-    void setTopicRestricted(bool topicRestricted);
-    bool getTopicRestricted() const;
-
-    void setKey(const std::string& key);
-    const std::string& getKey() const;
-
-    void setUserLimit(int limit);
+    //Méthodes INVITE
+    void addInvitedClient(unsigned int clientId);
+    void removeInvitedClient(unsigned int clientId);
+    bool isClientInvited(unsigned int clientId) const;
 };
--- include/Server.hpp
+++ include/Server.hpp
@@ -23,27 +23,36 @@ public:
     Server(int port, const std::string &password, struct tm *timeinfo);
     ~Server();
     Server(Server const& copy);
-    Server &operator=(Server const& copy);    
+    Server &operator=(Server const& copy);
+
     void run();
+
+    //Getters
     int const& getPort() const;
+    int getFdByNickname(const std::string &nickname);
+    std::string getDateTime() const;
+    Client* getClientByFd(int fd);
+    Channel* getChannelByName(const std::string& channelName);
+
+    //Setters
+    void setDatetime(struct tm *timeinfo);
+
+    //Méthode channels
     void addChannel(const std::string &channelName);
     void removeChannel(const std::string &channelName);
     void joinChannel(int fd, const std::string &channelName);
-    void kickClient(int fd, const std::string &channelName, int targetFd);
-    std::string getDateTime() const;
-    void setDatetime(struct tm *timeinfo);
     void broadcastToChannel(const std::string &channelName, const std::string &message, int senderFd);
-    int getFdByNickname(const std::string &nickname);
-    Client* getClientByFd(int fd);
-    Channel* getChannelByName(const std::string& channelName);
+
+    //Méthode clients
+    void kickClient(int fd, const std::string &channelName, int targetFd);
     bool checkDuplicateClient(std::string const nickClient);
 
 private:
     int server_fd;
-    std::vector<struct pollfd> pollfds;
     int port;
     std::string password;
     std::string _datetime;
+    std::vector<struct pollfd> pollfds;
     std::map<int, Client*> _ClientBook;  // Clé = FD, valeur = Client*
     std::map<std::string, Channel> _Channels;
 
