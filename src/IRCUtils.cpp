3eb6b7bb2c6db15b19af8acaa93bb450fa2a3483
akhamass <ayoubkhamassi@Host-001.lan>
2025-05-13 16:35:04 +0200

    version pushed for correction

--- src/IRCUtils.cpp
+++ src/IRCUtils.cpp
@@ -12,13 +12,3 @@ void sendResponse(Client* client, const std::string &response)
         std::cerr << "Erreur lors de l'envoi de réponse: " << strerror(errno) << "\n";
     }
 }
-
-//Fonction qui explique a un nouveau fd comment se register
-void displayRegistrationInstructions(Client *client)
-{
-    sendResponse(client, "You have to register to continue.");
-    sendResponse(client, "Please type :");
-    sendResponse(client, "PASS <password>");
-    sendResponse(client, "NICK <nickname>");
-    sendResponse(client, "USER <username>");
-}
